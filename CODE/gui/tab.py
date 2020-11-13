4d733dabc76e47c871d96e089d5567eec9c0881b
Kenny Bravo Rodriguez <kenny.bravo85@gmail.com>
2020-11-13 22:41:35 +0100

    Start Peptide Tab

---
+++ CODE/gui/tab.py
@@ -0,0 +1,38 @@
+# ------------------------------------------------------------------------------
+# Author: Kenny Bravo Rodriguez 2019 (kenny.bravorodriguez@mpi-dortmund.mpg.de)
+# 
+# Copyright (c) 2019 Max Planck Institute of Molecular Physiology
+#
+# This complete copyright notice must be included in any revised version of the
+# source code. Additional authorship citations may be added, but existing
+# author citations must be preserved.
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ------------------------------------------------------------------------------
+
+"""Individuals tabs for the App
+"""
+
+#region -------------------------------------------------------------- Imports
+import wx
+
+#endregion ----------------------------------------------------------- Imports
+
+#region ------------------------------------------------------------> Classess
+class Peptide(wx.Panel):
+	""""""
+
+
+#---
+#endregion ---------------------------------------------------------> Classess
